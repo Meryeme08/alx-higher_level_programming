a25698b200addea7d84a470679bf70d936170f94
Meryeme08 <bouchritmeryeme@gmail.com>
2023-11-20 15:51:19 -0800

    update

--- 0x05-python-exceptions/5-raise_exception.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def raise_exception():
-    name = "Musa"
-    try:
-        result = int(name)
-    except:
-        raise TypeError("")
--- 0x05-python-exceptions/6-raise_exception_msg.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def raise_exception_msg(message=""):
-    try:
-        if name:
-            pass
-    except:
-        raise NameError(message)
